b197424475169f796f9f182c9b072166ffa856ba
cradmlozzer <artem_pereverzev_04@bk.ru>
2023-10-07 13:26:39 +0300

    render..

--- main.py
+++ main.py
@@ -20,14 +20,14 @@ vk = vk_session.get_api()
 logging.info('vk_session is created')
 is_manual = False
 
-for event in longpoll.listen():
-    if not(is_manual):
-            vk.messages.send(
-                            user_id = event.user_id,
-                            random_id = 0,
-                            message=user_manual)
-            is_manual=True
+# for event in longpoll.listen():
+#     if not(is_manual):
+#             vk.messages.send(
+#                             user_id = event.user_id,
+#                             random_id = 0,
+#                             message=user_manual)
+#             is_manual=True
             
-    if event.type == VkEventType.MESSAGE_NEW and event.to_me:
-        manage_event(event=event, vk_api_method=vk, longpoll=longpoll)
+#     if event.type == VkEventType.MESSAGE_NEW and event.to_me:
+#         manage_event(event=event, vk_api_method=vk, longpoll=longpoll)
             
